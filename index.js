88eafee1a2afc1d75d89951b5f65c6e91d035522
Ataur Rahman <rahman.sharif39n@gmail.com>
2023-07-14 21:19:33 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const swaggetUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDoc = YAML.load('./blogApi.yml')
+
+const app = express()
+app.use(express.json())
+app.use('/docs',swaggetUI.serve,swaggetUI.setup(swaggerDoc))
+
+
+app.get('/status',(req,res)=>{
+    res.status(200).json({
+        status:"ok"
+    })
+})
+
+app.listen(5000,()=>{
+    console.log('server listening on port 5000');
+})
